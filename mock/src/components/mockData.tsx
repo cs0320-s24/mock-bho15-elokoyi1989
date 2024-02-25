ae98b561fc197407e8dbd830c5db890b6310a2e4
Brendan Ho <brendanho@brendansair682.devices.brown.edu>
2024-02-25 17:22:04 -0500

    refactor: deleted vanilla-mock from gearup

--- mock/src/components/mockData.tsx
+++
@@ -1,8 +0,0 @@
-export const getOutputForCommand = (command: string, verbose: boolean): string => {
-    switch (command) {
-        case "mode":
-            return "Verbose is now " + verbose + ".";
-        default:
-            return "Default output for unrecognized command";
-    }
-};
